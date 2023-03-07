5a8eadeeaacb275633aa32bf18633c21739d50f8
krnbr <krnbr@live.in>
2023-03-07 22:45:30 +0530

    init, for not requiring any downstream mock, like wiremock

--- src/main/java/in/neuw/self/UpstreamController.java
+++ src/main/java/in/neuw/self/UpstreamController.java
@@ -1,5 +1,7 @@
 package in.neuw.self;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,14 @@ import java.util.Date;
 @RestController
 public class UpstreamController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     RestTemplate restTemplate;
 
     @GetMapping("/v1/upstream")
     public Pong test() {
+        logger.info("upstream url called");
         return restTemplate.getForObject("/ping", Pong.class);
     }
 
