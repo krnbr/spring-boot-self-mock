1dbe73142776648c1aaecb196fa9d08ab5e6b892
krnbr <krnbr@live.in>
2023-03-08 11:38:58 +0530

    minor correction, removed packages

--- src/main/java/in/neuw/self/UpstreamController.java
+++ src/main/java/in/neuw/self/UpstreamController.java
@@ -2,12 +2,8 @@ package in.neuw.self;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Date;
 
 @RestController
 public class UpstreamController {
